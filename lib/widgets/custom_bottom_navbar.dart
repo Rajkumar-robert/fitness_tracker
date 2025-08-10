6834bf7c8ab868c7dd2aa216d11153928e6cae4a
Rajkumar-robert <rajkumar09112003@gmail.com>
2025-08-10 23:01:54 +0530

    Feat: add custom bottom navigation bar widget

---
+++ lib/widgets/custom_bottom_navbar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const CustomBottomNavBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedItemColor: Colors.deepPurple,
+      unselectedItemColor: Colors.grey[600],
+      showUnselectedLabels: true,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Progress'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+    );
+  }
+}
