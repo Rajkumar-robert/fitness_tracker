cd28920330270cb49887e9bf7a7a05f960080d7a
Rajkumar-robert <rajkumar09112003@gmail.com>
2025-08-15 22:55:34 +0530

    Feat: add method to fetch all users from the API

--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -19,3 +19,14 @@ class UserApi {
   }
 
   
+// Fetch all users
+  Future<List<User>> getAllUsers() async {
+    final response = await http.get(Uri.parse(_baseUrl));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((json) => User.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
